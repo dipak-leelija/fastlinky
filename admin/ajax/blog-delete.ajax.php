bf1ba40b8a56f58cde9463057b6f71233bf9a3e5
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-02-10 18:24:05 +0530

    Blog Deletion Completed From Admin

---
+++ admin/ajax/blog-delete.ajax.php
@@ -0,0 +1,27 @@
+<?php 
+session_start();
+
+require_once "../../_config/dbconnect.php";
+require_once "../../_config/dbconnect.trait.php";
+
+require_once "../../classes/date.class.php"; 
+require_once "../../classes/utility.class.php"; 
+require_once "../../classes/blog_mst.class.php";
+
+
+/* INSTANTIATING CLASSES */
+$dateUtil      	= new DateUtil();
+$utility		= new Utility();
+$blogMst		= new BlogMst();
+
+
+if (isset($_POST['delBlogId'])) {   
+    
+    $deleted	   = $blogMst->delBlogDtls($_POST['delBlogId']);
+    if($deleted){
+        echo 'true';
+    }else{
+        echo 'false';    }
+}
+	
+?>
