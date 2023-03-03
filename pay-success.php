8876489ac625b1e3eb0a2d5b897ee7003bac5a73
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-03-03 13:08:46 +0530

    Pay Success Page Updated

--- pay-success.php
+++ pay-success.php
@@ -311,21 +311,21 @@ if(isset($_SESSION['orderId'])) {
 }
 		
 ?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Payment Success - Order Received | <?php echo COMPANY_S; ?></title>
     <link rel="shortcut icon" href="<?php echo FAVCON_PATH?>" type="image/png"/>
     <link rel="apple-touch-icon" href="<?php echo FAVCON_PATH?>" />
     
-    <link rel="stylesheet" href="<?php echo URL ?>style/ansysoft.css" type="text/css" />
-    <link rel="stylesheet" href="plugins/bootstrap-5.2.0/css/bootstrap.css">
-    <link rel="stylesheet" href="plugins/fontawesome-6.1.1/css/all.css">
-    <link rel="stylesheet" href="css/leelija.css">
-    <link rel="stylesheet" href="css/payment-status.css">
+    <!-- <link rel="stylesheet" href="<?php echo URL ?>style/ansysoft.css" type="text/css" /> -->
+    <link rel="stylesheet" href="<?php echo URL ?>/plugins/bootstrap-5.2.0/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo URL ?>/plugins/fontawesome-6.1.1/css/all.css">
+    <link rel="stylesheet" href="<?php echo URL ?>/css/leelija.css">
+    <link rel="stylesheet" href="<?php echo URL ?>/css/payment-status.css">
 
 </head>
 
@@ -374,7 +374,7 @@ if(isset($_SESSION['orderId'])) {
     <!-- End  MainWrap -->
 
     <!-- Start Foter -->
-    <?php require_once "partials/footer.php"; ?>
+    <?php require_once ROOT_DIR."/partials/footer.php"; ?>
     <!-- End Foter -->
 </body>
 
