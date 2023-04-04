719b6395c35c4541af1c71ddf25bf8044c3725ae
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-04-04 13:04:59 +0530

    .php removed from index and customer cheacking file

--- includes/check-customer-login.inc.php
+++ includes/check-customer-login.inc.php
@@ -2,10 +2,10 @@
 
 if($cusId == 0){
     // redirect to login page if not loggedin 
-    header("Location: ".URL."/login.php");
+    header("Location: ".URL."/login");
 }elseif($cusDtl[0][0] == 2){
     // redirect to dashboard if loggedin user is set as seller 
-    header("Location: ".URL."/dashboard.php");
+    header("Location: ".URL."/dashboard");
 }
 
 ?>
