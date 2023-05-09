a7c44d818d535563400933852a942f325a9378e4
Rahul Majumdar <rahulmajumdar400@gmail.com>
2023-05-09 15:44:10 +0530

    location debugng

--- ajax/location.ajax.php
+++ ajax/location.ajax.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__DIR__) . "/includes/constant.inc.php";
+echo ROOT_DIR;
 require_once ROOT_DIR . "../_config/dbconnect.php";
 echo 'Line is here.';
 require_once ROOT_DIR . "../classes/utility.class.php";
