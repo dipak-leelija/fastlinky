b6f92d541a09c66a2540390430b29401ad41664e
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-07-01 11:32:35 +0530

    package order loging debuging

--- includes/check-customer-login.inc.php
+++ includes/check-customer-login.inc.php
@@ -3,10 +3,11 @@
 
 if($cusId == 0){
     // redirect to login page if not loggedin 
-    // header("Location: ".URL."/login");
+    // header("Location: ".");
     print_r($cusId);
     echo '<br>';
     print_r($cusDtl);
+    echo '<a href="'.URL.'"/login" >login </a>';
     exit;
 }elseif($cusDtl[0][0] == 2){
     // redirect to dashboard if loggedin user is set as seller 
