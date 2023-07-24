10e02e5b26636380136737d6b32f211625e70d6f
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-07-24 14:27:25 +0530

    paylater after payment completed for guest post

--- payments/paylater-pay-now.php
+++ payments/paylater-pay-now.php
@@ -2,8 +2,8 @@
 session_start();
 require_once dirname(__DIR__) .  "/includes/constant.inc.php";
 require_once ROOT_DIR . "/includes/paypal.inc.php";
-
-require_once ROOT_DIR ."/_config/dbconnect.php";
+require_once ROOT_DIR . "/includes/order-constant.inc.php";
+require_once ROOT_DIR . "/_config/dbconnect.php";
 
 require_once ROOT_DIR . "/classes/customer.class.php";
 require_once ROOT_DIR . "/classes/content-order.class.php";
@@ -32,9 +32,6 @@ require_once ROOT_DIR."/includes/check-customer-login.inc.php";
 
 $orderId = base64_decode($_GET['order']);
 
-// echo $clientName               = $_SESSION['name'];
-// exit;
-
 $showOrder  = $ContentOrder->clientOrderById($orderId);
 $orderPrice         = $showOrder['order_price'];
 $clientOrderedSite  = $showOrder['clientOrderedSite'];
@@ -46,7 +43,7 @@ $paymentStatus      = $ordTxn['transection_mode'];
 // print_r($paymentStatus);
 
 
-if ($paymentStatus != "Pay Later") {
+if ($paymentStatus != PAYLATER) {
     echo "This is Not a Paylater Order!";
 }
 
@@ -67,90 +64,78 @@ $_SESSION['payment-process'] = true;
     <link rel="shortcut icon" href="<?php echo FAVCON_PATH?>" type="image/png" />
     <link rel="apple-touch-icon" href="<?php echo FAVCON_PATH?>" />
 
-    <link rel="stylesheet" href="../plugins/bootstrap-5.2.0/css/bootstrap.css">
-    <link rel="stylesheet" href="../css/fontawesome-all.min.css">
-    <link rel="stylesheet" href="../css/payment-paylater-paynow.css">
+    <!-- <link rel="stylesheet" href="../css/payment-paylater-paynow.css"> -->
+
+    <link href="<?= URL ?>/plugins/bootstrap-5.2.0/css/bootstrap.css" rel="stylesheet">
+    <?php require_once ROOT_DIR.'/plugins/font-awesome/fontawesome.php'?>
+
+    <!-- custom css  -->
+    <link rel="stylesheet" href="<?= URL; ?>/css/payment-summary-style.css">
+
 </head>
 
 <body>
+    <section class="paylater-main-section">
+        <!-- logo codes -->
+        <div class="logos-section">
+            <div class="text-center">
+                <img src="<?php echo LOGO_WITH_PATH; ?>" alt="<?php echo COMPANY_FULL_NAME; ?>" class="main_logo">
+            </div>
+        </div>
+        <!-- cards for customers details -->
+        <div class="second-main-div-for-details">
+            <div class="row">
+                <div class="col-md-6 ">
+                    <div class="customer-details-section">
+                        <div class="card customer-d-card">
+                            <h5>Customer Details</h5>
 
-    <section class="container mt-5" style="display: none;" id="waiting-box">
-        <p class="text-center text-warning py-5 mb-0">
-            <img src="../images/icons/loading.gif" alt="">
-            Don't Click anywhere or go back, redirecting you to the payment page...
-        </p>
-    </section>
-
-    <section class="paylater-paynow-section " id="payment-box">
-        <div class="border p-2 p-sm-4 mt-2  bg-light shadow rounded">
-            <div class="row m-0 w-100 justify-content-center">
-                <div class="col-12 py-3 d-flex justify-content-center mb-4">
-                    <img src="../images/logo/logo.png" class="page-logo" alt="">
-                </div>
-                <!-- <div class="row justify-content-between"> -->
-                    <div class="col-sm-6">
-                        <h5 class="">Customer Details:</h5>
-                        <div class="">
-                            <p class="mb-0" id="customer-name"> <span class="title-span">NAME: </span> <?php echo $buyer[0][5].' '.$buyer[0][6];?></p>
-                            <p class="mb-0">
-                                <?php
-                                if($buyer[0][24] != null){
-                                        echo $buyer[0][24];
-                                    }
-                                    
-                            ?>
-                            </p>
-                            <p class="mb-0">  <?php
-                                if($buyer[0][25] != null){
-                                        echo $buyer[0][25].', ';
-                                    }
-
-                                if ($buyer[0][26] != null) {
-                                        echo $buyer[0][26];
-                                    }
-                                    
-                            ?>
-                            </p>
-                            <p class="mb-0">
-                                <?php
-                                if($buyer[0][27] != null){
-                                        echo $buyer[0][27];
-                                    }
-                            ?>
-                            </p>
-                            <p class="mb-0">
-                                <?php
-                                if($buyer[0][28] != null){
-                                        echo $buyer[0][28].', ';
-                                    }
-                                if($buyer[0][29] != null){
-                                    echo $buyer[0][29];
-                                }
-                            ?>
+                            <p class="mb-0" id="customer-name"> <span class="title-span">NAME: </span>
+                                <?php echo $buyer[0][5].' '.$buyer[0][6];?>
                             </p>
                             <p class="mb-0"><span class="title-span">ADDRESS: </span>
+
                                 <?php
+
                                 if($buyer[0][30] != null){
                                     $country = $Location->getCountyById($buyer[0][30]);
-                                        echo $country['name'];
-                                    }
-                            ?>
+                                    $countryName = $country['name'];
+                                }
+
+                                $addressArr = array(
+                                                    'address1' => $buyer[0][24],
+                                                    'address2' => $buyer[0][25],
+                                                    'address3' => $buyer[0][26],
+                                                    'city' => $buyer[0][27],
+                                                    'state' => $buyer[0][28],
+                                                    'country' => $countryName,
+                                                    'zipcode' => $cusDtl[0][29]            
+                                                    );
+                                $Location->printAddress($addressArr);
+                                ?>
                             </p>
+
+
                         </div>
                     </div>
-
-                    <div class="col-sm-6 text-sm-end mt-sm-0 mt-3 ">
-                        <h5 class="">Order Details:</h5>
-                        <p class="mb-0"><span class="title-span">ORDER ID:</span> <?php echo '#'.$orderId; ?></p>
-                        <p class="mb-0"><span class="title-span">ORDER DATE:</span>
-                            <?php echo $DateUtil->dateText($orderDate); ?></p>
+                </div>
+                <div class="col-md-6">
+                    <div class="Client-details-section">
+                        <div class="card customer-d-card">
+                            <h5>Bill Details</h5>
+                            <p class="mb-0"><span class="title-span">ORDER ID:</span> <?php echo '#'.$orderId; ?></p>
+                            <p class="mb-0"><span class="title-span">ORDER DATE:</span>
+                                <?php echo $DateUtil->dateText($orderDate); ?></p>
+                        </div>
                     </div>
-                <!-- </div> -->
-                <hr style="width: 95%;" class="mt-3 mt-md-5">
+                </div>
+            </div>
 
-                <div class="col-12 table-responsive mt-3 mt-md-5">
-                    <table class="table table-sm  table-bordered align-middle text-center ">
-                        <thead>
+            <form method="post" id="orderForm">
+
+                <div class=" display-table text-center">
+                    <table class="table detailing-table">
+                        <thead class="table-secondary">
                             <tr>
                                 <th scope="col">Order Id</th>
                                 <th scope="col">Site Name</th>
@@ -165,24 +150,61 @@ $_SESSION['payment-process'] = true;
                                 <td><?php echo $orderPrice; ?></td>
                                 <td><?php echo $orderPrice; ?></td>
                             </tr>
-                            <tr>
-                                <th colspan="3" scope="row">Total</th>
-                                <td><b><?php echo $orderPrice; ?></b></td>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
 
-                <div class="col-12">
-                    <div class="d-flex justify-content-center justify-content-md-end mt-2" id="payBtn">
-                        <div id="paypal-payment-button">
+                <!-- invoice table -->
+                <div class="mt-5">
+                    <div class="row" style="display: flex; justify-content: end;">
+                        <div class="col-md-5  ">
+                            <table class="table table-responsive">
+                                <thead class="table-secondary">
+                                    <tr>
+                                        <th class="text-center" scope="col">Invoice Summary</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <div class="row">
+                                                <div class="col-6">Total</div>
+                                                <div class="col-6 text-end fw-semibold">
+                                                    <?= CURRENCY; ?> <span id="amount"><?= $orderPrice;?></span>
+                                                </div>
+                                            </div>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div class="row">
+                                                <div class="col-12 text-end" id="payBtn">
+                                                    <div id="paypal-payment-button">
+                                                    </div>
+                                                    <div class="col-12 text-end mb-1">
+                                                        <button type="button"
+                                                            class="btn rounded-pill w-100 fw-bolder pay_button"
+                                                            onclick="paylaterOrder()">
+                                                            PayLater
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 text-center" id="payBtn">
+                                                    <a href="#" onclick="history.back()">Cancel</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     </section>
-
 
     <form action="paylater-pay-success.php" method="post" id="send-data" class="d-none">
         <input type="text" name="data" id="form-inp">
@@ -194,19 +216,16 @@ $_SESSION['payment-process'] = true;
         src="https://www.paypal.com/sdk/js?client-id=<?= PAYPAL_CLIENT_ID ?>&disable-funding=credit,card&currency=USD">
     </script>
     <script>
-    let customerName = document.getElementById("customer-name").innerText;
-    let waitingBox = document.getElementById("waiting-box");
-    let paymentBox = document.getElementById("payment-box");
-
     let formInp = document.getElementById('form-inp');
     let form = document.getElementById('send-data');
 
 
     paypal.Buttons({
         style: {
+            height: 45,
             layout: 'vertical',
             color: 'gold',
-            shape: 'rect',
+            shape: 'pill',
             label: 'paypal'
         },
         createOrder: function(data, actions) {
@@ -226,8 +245,6 @@ $_SESSION['payment-process'] = true;
 
                 document.getElementById('payBtn').innerHTML =
                     `<div class="bg-secondary border border-info rounded text-center"><p class="fw-bold py-2 mb-0"><span><img src="../images/icons/loading-2.gif" alt="loading"></span> Please Wait..</p></div>`;
-                waitingBox.style.display = "block";
-                paymentBox.style.display = "none !import";
                 formInp.value = JSON.stringify(details);
                 form.submit();
 
