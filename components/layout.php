720e5658f024fdcfd22f96cf8269581b565f9bb5
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-02-17 15:07:05 +0530

    layout designing

--- components/layout.php
+++ components/layout.php
@@ -1,10 +1,10 @@
-<section class="mt-5">
-    <div class="d-flex justify-content-between layoutdiv h-100">
-        <div class="w-50 imgdiv">
+<section class="lets-talk-section py-0">
+    <div class="row justify-content-between layoutdiv h-100">
+        <div class="col-12 col-md-3 p-0 order-2 order-md-1 imgdiv">
             <img src="https://optimise2.assets-servd.host/obedient-zorilla/production/images/Universal-cool-graphics/footer-3D.png?w=1600&q=80&fm=webp&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.5&dm=1653141251&s=c1bdfa6eac6fcf827bfa2426c2dc5630" alt="">
         </div>
-        <div class="w-50 mr-5 mt-4">
-            <h2 class="mt-5">Curious? <br> Let’s talk link building</h2>
+        <div class="col-12 col-md-8 order-1 order-md-2 mt-0 p-5 p-md-2">
+            <h2 class="fw-semibold">Curious? <br> Let’s talk link building</h2>
             <div class="max-w-lg mb-4">
                 <div class="redactor text-gray-800 w-75">We love talking link building. Let us know about your project and we'll send you a free proposal</div>
             </div>
