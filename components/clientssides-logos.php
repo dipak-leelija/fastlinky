1e2f9ab435e3cd2e174470e9fea5f0e7cac82f61
Jayanta Barman <jbleelija@gmail.com>
2024-02-22 15:24:52 +0530

    modify this page

--- components/clientssides-logos.php
+++ components/clientssides-logos.php
@@ -125,14 +125,14 @@
         </div>
         <div class="swiper mySwiper mt-5">
             <div class="swiper-wrapper align-item-center"  style="width: 186px;">
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/cc1.png" class="img-responsive" alt=""></div>
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"> <img src="images/clients-logos/icademy1.png" class="img-responsive" alt=""></div>
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/specscart3.png " class="img-responsive" alt=""></div>
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"> <img src="images/clients-logos/ISB3.png" class="img-responsive" alt=""></div>
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/cs1.png" class="img-responsive" alt=""></div>
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"> <img src="images/clients-logos/khatabook3.png" class="img-responsive" alt=""></div>
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/namecheap1.png" class="img-responsive" alt=""></div>
-                <div class="swiper-slide" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/recovery3.png" class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/cc1.png" class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"> <img src="images/clients-logos/icademy1.png" class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/specscart3.png " class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"> <img src="images/clients-logos/ISB3.png" class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/cs1.png" class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"> <img src="images/clients-logos/khatabook3.png" class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/namecheap1.png" class="img-responsive" alt=""></div>
+                <div class="swiper-slide shadow-sm rounded" style="width: 186px;margin-left:1px;"><img src="images/clients-logos/recovery3.png" class="img-responsive" alt=""></div>
             </div>
             <!-- <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div> -->
